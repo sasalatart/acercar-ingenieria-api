1935b1a708a000198af8f27b1164c2c1e26d3e71
Sebastián Salata R-T <sa.salatart@gmail.com>
2018-03-20 22:49:06 -0300

    🐛  fix ExceptionHandler json_response usage

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -3,15 +3,15 @@ module ExceptionHandler
 
   included do
     rescue_from ActiveRecord::RecordNotFound do |e|
-      json_response({ message: e.message }, :not_found)
+      json_response({ message: e.message }, status: :not_found)
     end
 
     rescue_from ActiveRecord::RecordInvalid do |e|
-      json_response({ message: e.message }, :unprocessable_entity)
+      json_response({ message: e.message }, status: :unprocessable_entity)
     end
 
     rescue_from CanCan::AccessDenied do |e|
-      json_response({ message: e.message }, :unauthorized)
+      json_response({ message: e.message }, status: :unauthorized)
     end
   end
 end
