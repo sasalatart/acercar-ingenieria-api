f5f0450ef5b88b43036796a26be5addc7e71d88e
Sebastián Salata R-T <sa.salatart@gmail.com>
2018-04-15 11:50:38 -0300

    🐛  only sanitize changed attributes

--- app/models/concerns/sanitizable.rb
+++ app/models/concerns/sanitizable.rb
@@ -1,7 +1,7 @@
 module Sanitizable
   def sanitize(*properties)
     properties.each do |attr|
-      next unless self[attr]
+      next unless self[attr] && will_save_change_to_attribute?(attr.to_sym)
       self[attr] = Sanitize.fragment(self[attr], Sanitize::Config::RELAXED)
     end
   end
