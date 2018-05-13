de85d19b68a4bdd620ca898ecbeca96254dc5384
Sebastián Salata R-T <sa.salatart@gmail.com>
2018-05-13 11:34:50 -0400

    ♻️ check for 'update' & 'destroy' authorizations for VideoLinks via attributes instead of params

--- app/lib/abilities/video_links_ability.rb
+++ app/lib/abilities/video_links_ability.rb
@@ -4,7 +4,8 @@ module Abilities
       super(user, params)
 
       unless @majors_user_is_admin.empty?
-        can %i[create update destroy], VideoLink if @majors_user_is_admin.include? params[:major_id].to_i
+        can %i[create], VideoLink if @majors_user_is_admin.include? params[:major_id].to_i
+        can %i[update destroy], VideoLink, video_linkable_type: Major.name, video_linkable_id: @majors_user_is_admin
       end
 
       can %i[index], VideoLink
