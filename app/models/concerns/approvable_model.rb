23115f9dd5a69b219b4c97ef4516565452d3ec2b
Sebastián Salata R-T <sa.salatart@gmail.com>
2018-05-26 09:46:20 -0400

    ♻️ update comments' approval in after_save callback instead of within update_approval method

--- app/models/concerns/approvable_model.rb
+++ app/models/concerns/approvable_model.rb
@@ -1,12 +1,16 @@
 module ApprovableModel
-  def update_approval(value, user)
-    update_attribute(:approved, value)
-    notify_approval_changed(value, user)
+  extend ActiveSupport::Concern
 
-    return unless respond_to?(:comments)
-    Comment.where(commentable: [self, comments])
-           .update_all(approved_commentable: value)
+  included do
+    after_save :verify_comments_approval
   end
+
+  def update_approval(value, user)
+    update!(approved: value)
+    notify_approval_changed(value, user)
+  end
+
+  private
 
   def notify_approval_changed(value, user)
     type = NotifyableModel::NOTIFICATION_TYPES[value ? :approved : :rejected]
@@ -14,5 +18,11 @@ module ApprovableModel
     user_ids += major.admins.pluck(:id) if respond_to?(:major_id) && major_id
     user_ids += [author_id] if respond_to?(:author_id)
     notify(type, user.id, user_ids)
+  end
+
+  def verify_comments_approval
+    return unless saved_change_to_approved? && respond_to?(:comments)
+    Comment.where(commentable: [self, comments])
+           .update_all(approved_commentable: approved)
   end
 end
